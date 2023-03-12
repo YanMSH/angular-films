857853e3ccfaf6b16af091856011d09fe2b761c1
Yan Sharipov <89582326+YanMSH@users.noreply.github.com>
2023-03-12 11:47:40 +0500

    fix: make loading-message a standalone component

--- src/app/components/loading-message/loading-message.component.ts
+++ src/app/components/loading-message/loading-message.component.ts
@@ -2,8 +2,18 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-loading-message',
-  templateUrl: './loading-message.component.html',
-  styleUrls: ['./loading-message.component.css']
+  template: `<p class="loading-message">Загрузка...</p>`,
+  styles: [`.loading-message {
+    width: 100vw;
+    height: 100vh;
+    text-align: center;
+    line-height: 75vh;
+    font-size: 48px;
+    font-family: Montserrat, sans-serif;
+    font-weight: 600;
+  }`],
+  standalone: true,
+  imports: [LoadingMessageComponent]
 })
 export class LoadingMessageComponent {
 
